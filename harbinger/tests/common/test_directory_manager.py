fc44ae221ae5d148ed914d52c1ea91e4ee2e4f8d
Douglas Aaser <da519m@att.com>
2018-09-25 14:59:49 -0400

    add skeleton code

---
+++ harbinger/tests/common/test_directory_manager.py
@@ -0,0 +1,8 @@
+# This Python file uses the following encoding: utf-8
+
+import unittest
+
+from harbinger.common import directory_manager
+
+class DirectoryManagerTest(unittest.TestCase):
+    pass
