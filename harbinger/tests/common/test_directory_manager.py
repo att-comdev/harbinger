d64324b677cc075ada08d9d8facd7c080fd4fe9a
Douglas Aaser <da519m@att.com>
2018-09-26 15:38:09 -0400

    finished test_setup - coverage at 57%

--- harbinger/tests/common/test_directory_manager.py
+++ harbinger/tests/common/test_directory_manager.py
@@ -1,8 +1,34 @@
-# This Python file uses the following encoding: utf-8
-
+import argparse
 import unittest
 
-from harbinger.common import directory_manager
+import mock
 
-class DirectoryManagerTest(unittest.TestCase):
-    pass
+from harbinger.base import Base  # noqa
+from harbinger.common.directory_manager import \
+    DirectoryManager
+
+
+class TestDirectoryManager(unittest.TestCase):
+
+    def setUp(self):
+        self.args = mock.Mock(spec=argparse.Namespace)
+        self.test_object = DirectoryManager()
+
+    @mock.patch('harbinger.common.directory_manager.os', autospec=True)
+    def test_setup(self, mock_os):
+        mock_os.path.isdir.return_value = False
+
+        self.test_object.setup()
+
+        calls = [
+            mock.call(self.test_object.inputs_dir),
+            mock.call(self.test_object.outputs_dir)
+        ]
+
+        mock_os.path.isdir.assert_has_calls(calls)
+        mock_os.makedirs.assert_has_calls(calls)
+
+    @mock.patch('harbinger.common.directory_manager.os', autospec=True)
+    def test_archive_outputs(self, mock_os):
+        self.test_object.setup()
+        self.test_object.archive_outputs()
