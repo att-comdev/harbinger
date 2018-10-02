86919c0d6e14f411dff70682c6ddfebcd61cbc50
Douglas Aaser <da519m@att.com>
2018-10-02 08:05:19 -0400

    brought factory unit tests up to 100% coverage

---
+++ harbinger/tests/factory/test_options.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import unittest
+
+import mock
+
+from harbinger.base import Base
+from harbinger.factory.options import Options
+
+
+class TestOptions(unittest.TestCase):
+    """Unit tests for Options"""
+
+    def setUp(self):
+        self.args = mock.Mock(spec=argparse.Namespace)
+        self.yaml = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                 "data/test_yaml.yaml")
+
+        self.base_object = Base(app=mock.Mock(), app_args=self.args)
+        self.yaml_file = self.base_object.load_yaml(self.yaml)
+
+    def test__init(self):
+        self.test_object = Options(self.yaml_file["Options"])
+        self.assertEqual("myTestProject", self.test_object.project)
