07fbce7c98a1f6a743235efe45dc498403ad4965
Douglas Aaser <da519m@att.com>
2018-10-03 14:49:21 -0400

    fixed tox issues

--- harbinger/tests/flavors/test_flavor_manager.py
+++ harbinger/tests/flavors/test_flavor_manager.py
@@ -6,50 +6,41 @@ from testfixtures import log_capture
 
 from harbinger.base import Base
 from harbinger.flavors.flavor_manager import FlavorManager
-from harbinger.factory.options import Options
-from harbinger.factory.environment import Environment
 
 
 class TestFlavorManager(unittest.TestCase):
 
-    @mock.patch.object(Options, '__init__')
-    @mock.patch.object(Environment, '__init__')
-    def setUp(self, mock_env, mock_options):
+    def setUp(self):
         self.args = mock.Mock(spec=argparse.Namespace)
         self.base_object = Base(app=mock.Mock(), app_args=self.args)
         self.test_object = FlavorManager(
-            "framework",
-            mock_env.OS_AUTH_URL +
-            mock_env.OS_API_VERSION,
-            mock_options.username,
-            mock_options.password,
-            mock_options.project
-        )
-
-    @log_capture()
-    @mock.patch('harbinger.flavors.flavor_manager.FlavorManager',
-                autospec=True)
-    def test__init(self, mock_flavor, capture):
-        test_object = FlavorManager(
             "framework", "example.com/fake",
             "username", "password", "myProject"
         )
 
-        capture.check_present(
-            ('harbinger.flavors.flavor_manager', 'INFO',
-            'Creating Nova client (for flavor mgmt) for framework '
-            'with parameters: keystone_endpoint: example.com/fake,'
-            ' username: username, project_name: myProject')
-        )
+    @log_capture()
+    def test__init(self, capture):
+        with mock.patch('harbinger.flavors.flavor_manager.FlavorManager'):
+            FlavorManager(
+                "framework", "example.com/fake",
+                "username", "password", "myProject"
+            )
+
+            capture.check_present(
+                ('harbinger.flavors.flavor_manager', 'INFO',
+                 'Creating Nova client (for flavor mgmt) for framework '
+                 'with parameters: keystone_endpoint: example.com/fake,'
+                 ' username: username, project_name: myProject')
+            )
 
     @log_capture()
     def test_check_flavor(self, capture):
         self.test_object.nova = mock.MagicMock()
 
         retval = [mock.MagicMock() for m in range(10)]
-        
-        for i, m in enumerate(retval):
-            type(m).name = mock.PropertyMock(return_value='f'+str(i))
+
+        for i, mock_obj in enumerate(retval):
+            type(mock_obj).name = mock.PropertyMock(return_value='f' + str(i))
 
         self.test_object.nova.flavors.list.return_value = retval
 
@@ -60,13 +51,13 @@ class TestFlavorManager(unittest.TestCase):
 
         capture.check(
             ('harbinger.flavors.flavor_manager',
-            'INFO', 'Flavor <f1> exists: True'),
+             'INFO', 'Flavor <f1> exists: True'),
             ('harbinger.flavors.flavor_manager',
-            'INFO', 'Flavor <f6> exists: True'),
+             'INFO', 'Flavor <f6> exists: True'),
             ('harbinger.flavors.flavor_manager',
-            'INFO', 'Flavor <f10> exists: False'),
+             'INFO', 'Flavor <f10> exists: False'),
             ('harbinger.flavors.flavor_manager',
-            'INFO', 'Flavor <f38439> exists: False')
+             'INFO', 'Flavor <f38439> exists: False')
         )
 
     @log_capture()
@@ -75,6 +66,6 @@ class TestFlavorManager(unittest.TestCase):
         self.test_object.create_flavor('f1', '2048', '4', '1')
         capture.check(
             ('harbinger.flavors.flavor_manager', 'INFO',
-            'Creating flavor f1')
+             'Creating flavor f1')
         )
         self.test_object.nova.flavors.create.assert_called_once()
